a7b1d9819c4a81665caa0be3a34e703c03effd60
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2023-11-16 17:42:18 +0530

    Update main.tf

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -4,35 +4,29 @@ resource "aws_instance" "example" {
   instance_type        = "t2.micro"
   subnet_id            = element(var.public_subnet_ids, count.index)
   vpc_security_group_ids = [var.ec2_sg_id]
-   # Use vpc_security_group_ids instead
 
-  tags = {
-    Name = "ExampleInstance-${count.index + 1}"
-  }
-}
+  user_data = <<-EOF
+    #!/bin/bash
+    # Install CodeDeploy agent and Docker
+    sudo apt-get update -y
+    sudo apt-get install -y ruby wget
 
- user_data = <<-EOF
-              #!/bin/bash
-              # Install CodeDeploy agent and Docker
-              sudo apt-get update -y
-              sudo apt-get install -y ruby wget
+    # Install CodeDeploy agent
+    cd /home/ubuntu
+    wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+    chmod +x ./install
+    sudo ./install auto
 
-              # Install CodeDeploy agent
-              cd /home/ubuntu
-              wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-              chmod +x ./install
-              sudo ./install auto
-
-              # Install Docker
-              sudo apt-get install -y docker.io
-              sudo usermod -aG docker ubuntu
-              sudo systemctl enable docker
-              sudo systemctl start docker
-              # Install AWS CLI
-              sudo apt-get install -y awscli
-              # Install mysql
-              sudo apt install mysql-client-core-8.0
-              EOF
+    # Install Docker
+    sudo apt-get install -y docker.io
+    sudo usermod -aG docker ubuntu
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    # Install AWS CLI
+    sudo apt-get install -y awscli
+    # Install mysql
+    sudo apt install mysql-client-core-8.0
+    EOF
 
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
