4766b66b8bd212a9f25898138679077bab19da8a
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2023-11-29 00:12:07 +0530

    Update main.tf

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -1,3 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "statefile-terraform-bucket" # change this
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
 resource "aws_instance" "example" {
   count                = 2
   ami                  = "ami-0fc5d935ebf8bc3bc"
